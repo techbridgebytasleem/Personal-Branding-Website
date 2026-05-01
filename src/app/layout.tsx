46410be2074f335a1494d4d78b8ab68a4fb592b1
techbridgebytasleem <techbridgebytasleem@gmail.com>
2026-05-01 13:04:33 +0530

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,7 @@ export default function RootLayout({
     <html lang="en" className={`${plusJakartaSans.variable} ${fraunces.variable}`}>
       <body className={plusJakartaSans.className}>
         {children}
-
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Ftasleemban4009back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.18" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+      </body>
     </html>
   );
 }
